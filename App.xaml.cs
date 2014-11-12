8ba5b06103f14c7abfc9ad07fb10d1279991dc2a
migteje <miguelangeltjr@gmail.com>
2014-11-12 12:14:08 +0100

    Interface Added
    ColorBones with a new implemented interface and redesign

---
+++ App.xaml.cs
@@ -0,0 +1,19 @@
+﻿
+namespace KinectInterfaz
+{
+    using Microsoft.Kinect.Wpf.Controls;
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Data;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Windows;
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        internal KinectRegion KinectRegion { get; set; }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿
+namespace KinectInterfaz
+{
+
+    using Microsoft.Kinect;
+    using Microsoft.Kinect.Wpf.Controls;
+    using KinectInterfaz.DataModel;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
+
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            KinectRegion.SetKinectRegion(this, kinectRegion);
+
+            App app = ((App)Application.Current);
+            app.KinectRegion = kinectRegion;
+
+            // Use the default sensor
+            this.kinectRegion.KinectSensor = KinectSensor.GetDefault();
+
+            //// Add in display content
+            var sampleDataSource = SampleDataSource.GetGroup("Group-1");
+            this.itemsControl.ItemsSource = sampleDataSource;
+        }
+
+        /// <summary>
+        /// Handle a button click from the wrap panel.
+        /// </summary>
+        /// <param name="sender">Event sender</param>
+        /// <param name="e">Event arguments</param>
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            var button = (Button)e.OriginalSource;
+            SampleDataItem sampleDataItem = button.DataContext as SampleDataItem;
+
+            if (sampleDataItem != null && sampleDataItem.NavigationPage != null)
+            {
+                backButton.Visibility = System.Windows.Visibility.Visible;
+                navigationRegion.Content = Activator.CreateInstance(sampleDataItem.NavigationPage);
+            }
+            else
+            {
+                var selectionDisplay = new SelectionDisplay(button.Content as string);
+                this.kinectRegionGrid.Children.Add(selectionDisplay);
+
+                // Selection dialog covers the entire interact-able area, so the current press interaction
+                // should be completed. Otherwise hover capture will allow the button to be clicked again within
+                // the same interaction (even whilst no longer visible).
+                selectionDisplay.Focus();
+
+                // Since the selection dialog covers the entire interact-able area, we should also complete
+                // the current interaction of all other pointers.  This prevents other users interacting with elements
+                // that are no longer visible.
+                this.kinectRegion.InputPointerManager.CompleteGestures();
+
+                e.Handled = true;
+            }
+        }
+
+        /// <summary>
+        /// Handle the back button click.
+        /// </summary>
+        /// <param name="sender">Event sender</param>
+        /// <param name="e">Event arguments</param>
+        private void GoBack(object sender, RoutedEventArgs e)
+        {
+            backButton.Visibility = System.Windows.Visibility.Hidden;
+            navigationRegion.Content = this.kinectRegionGrid;
+        }
+    }
+}
